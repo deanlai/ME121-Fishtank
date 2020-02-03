fbdc357b281a483704ba8adf05626356e6683666
Sean <sean.j.lai@gmail.com>
2020-02-02 18:52:24 -0800

    renamed to sensor_reading_lcd.ino, and inside folder of same name, minor edits to comments/code structure

--- Fishtank_code/sensor_reading_lcd/sensor_reading_lcd.ino
+++ Fishtank_code/sensor_reading_lcd/sensor_reading_lcd.ino
@@ -1,23 +1,27 @@
 //--------------------------------------------------------
-// Sensor readings program with conversion from analog output to wt% salinity
+// Sensor readings program for conversion from analog output to salinity (wt%)
 // Version 1.1
 // 1/27/20
 // The North American Council on Aquatic Housing and Development
 // NOTE: To run this code you must have downloaded the LiquidCrystal_I2C library (and added it to the sketch from the IDE), 
-// which is in Whitman's folder in the main directory
+//       which is in Whitman's folder in the main directory
 //--------------------------------------------------------
+
+// Setup LCD screen
+#include <Wire.h> // Include wire (i2c comm) and liquidcrystal libraries 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // 20x4 LCD screen, 0x27 is the address
 
 // delcare global pins
 const int SALINITY_POWER_PIN = 8;
-#include <Wire.h> //Include wire (i2c comm) and liquidcrystal libraries 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); //20x4 LCD screen, 0x27 is the address 
 
 void setup(){
-pinMode(SALINITY_POWER_PIN, OUTPUT);
-Serial.begin(9600);
-lcd.init(); //initiate LCD
-lcd. backlight(); //backlight on
+    // Setup pins and serial comms
+    pinMode(SALINITY_POWER_PIN, OUTPUT);
+    Serial.begin(9600);
+
+    lcd.init(); // initiate LCD
+    lcd.backlight(); // backlight on
 
 }
 
@@ -25,12 +29,14 @@ void loop(){
     // declare local pins
     const int SALINITY_READING_PIN = A0;
 
+    // declare constants from power law fit
+    const float c1 = 944.3844;
+    const float c2 = 0.1368; // constant inverse to solve for salinity
+
     // setup variables
     int numReadings = 30;
     int salinityReading = 0;
     float voltage = 0.0;
-    float c1 = 944.3844;
-    float c2 = 1/0.1368; //constant inverse to solve for salinity
     float salinityPercentage, consteq;
 
     // take a salinity reading
@@ -38,36 +44,30 @@ void loop(){
     voltage = salinityReading*5/1023.0;
     int currentTime = millis();
     
-    // Convert from analog to salinity percentage
-    consteq = (salinityReading/c1);
-    salinityPercentage = pow(consteq, c2);
-
-    /* Comment out whichever Serial dialog is not needed
-    // Print to Serial Monitor (for humans)
-    Serial.print("Time: ");
-    Serial.print(currentTime);
-    Serial.print("  |  Voltage: ");
-    Serial.println(voltage);
-    */
+    // Convert from analog to salinity percentage using s = (a/c1)^(1/c2)
+    salinityPercentage = pow(salinityReading/c1, 1/c2);
 
     // Print to Serial Monitor (for data analysis)
     Serial.print(salinityReading);
     Serial.print(" ");
-    Serial.print(salinityPercentage);
+    Serial.print(salinityPercentage, 4);
     Serial.println();
 
-    //Print to LCD Screen
-    lcd.setCursor(0,1);
+    // Print to LCD Screen
+    lcd.setCursor(0,1); // Print to second row
     lcd.print("Percent salt: ");
-    lcd.print(salinityPercentage, 4); //to 4 decimal places accuracy
-    lcd.setCursor(0, 2);
+    lcd.print(salinityPercentage, 4); // to 4 decimal places accuracy
+    lcd.setCursor(0, 2); // Print to third row
     lcd.print("Analog read: ");
     lcd.print(salinityReading);
-    delay(250); //delay between refresh
+    delay(250); // delay between refresh
      
 }
 
 float takeReading(int powerPin, int readingPin, int numReadings){
+    // input: powerPin (digital out), readingPin (analog in), numReadings (>0)
+    // output: average of numReadings readings
+
     float sum = 0.0;
     digitalWrite(powerPin, 1); // Turn sensor on
     delay(100); // Allow power to settle
