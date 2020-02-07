d37e22d8949072a003eda53f99e83bbf37e33d3d
10bulls10 <sspitzer@pdx.edu>
2020-02-07 13:38:44 -0800

    buttontoggle example

---
+++ Whitman/userdefbuttontoggle/userdefbuttontoggle.ino
@@ -0,0 +1,41 @@
+
+const int buttonin=5;
+const int LEDPin = 6;
+int state = LOW;
+int reading;
+int previous=LOW;
+int buttoncount;
+
+long timer = 0;
+long debounce = 200;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(buttonin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+
+}
+
+void loop() {
+
+  int bstate = buttonRead(buttonin);
+  digitalWrite(LEDPin, bstate);
+}
+
+int buttonRead(int buttonin){
+    reading = digitalRead(buttonin);
+  
+  if(reading==HIGH && previous==LOW && (millis()-timer*1000)>debounce){
+    if (state == LOW){
+      state = HIGH;
+    }
+    else {
+      state = LOW;
+    }
+    timer = millis()/1000;
+  } 
+  digitalWrite(LEDPin, state);
+  previous = reading;
+  return state;
+}
