e931a0fe3c06b5aa48238930dc739155b2c34871
10bulls10 <sspitzer@pdx.edu>
2020-02-10 10:42:38 -0800

    lcd work

--- LCD/userDefinedControlv_1.0/userDefinedControlv_1.0.ino
+++ LCD/userDefinedControlv_1.0/userDefinedControlv_1.0.ino
@@ -1,8 +1,9 @@
 
+
 // Setup LCD screen
 #include <Wire.h> // Include wire (i2c comm) and liquidcrystal libraries
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // 20x4 LCD screen, 0x27 is the address
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // object declaration 20x4 LCD screen, 0x27 is the address
 
 void setup() {
   Serial.begin(9600);
@@ -14,6 +15,7 @@ void loop() {
 }
 
 void lcdSetup(){
+
   lcd.init();
   lcd.backlight();
   
