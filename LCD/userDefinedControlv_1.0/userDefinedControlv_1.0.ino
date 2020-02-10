72fbeab5ea8f291e504166058bd7d745cd475913
10bulls10 <sspitzer@pdx.edu>
2020-02-10 11:38:01 -0800

    fancy startup

--- LCD/userDefinedControlv_1.0/userDefinedControlv_1.0.ino
+++ LCD/userDefinedControlv_1.0/userDefinedControlv_1.0.ino
@@ -7,28 +7,40 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // object declaration 20
 
 void setup() {
   Serial.begin(9600);
-  lcdSetup();
+  
 }
 
 void loop() {
-
+  lcdSetup();
+  
 }
 
 void lcdSetup(){
-
-  int cpos = 17;
+  int ppos = 4;
+  int cpos = 4;
   int npos = 0;
+  String nacahd = "N.A.C.A.H.D.";
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(16,1);
-  lcd.print("N.A.C.A.H.D.");
-  String nacahd = "N.A.C.A.H.D.";
-  for (int poscount=0; poscount<5; poscount++){
-    lcd.scrollDisplayLeft();
+  lcd.setCursor(13,1);
+  for (int poscount=0; poscount<6; poscount++){
+    lcd.setCursor(ppos, 1);
+    lcd.print(" .");
+    ppos += 2;
+    delay(200);
+    }
+  for (int poscount=0; poscount<12; poscount++){
+    //lcd.scrollDisplayLeft();
     lcd.setCursor(cpos, 1);
     lcd.print(nacahd.charAt(npos));
     npos += 1;
-    cpos -= 1;
-    delay(100);
+    cpos += 1;
+    delay(200);
   }
+}
+
+void lcdUpdate () {
+
+
+  
 }
