f749aaff76d4a170e09180e7a36d195842280888
10bulls10 <sspitzer@pdx.edu>
2020-02-10 12:22:47 -0800

    commented the fancy startup code

---
+++ LCD/useDefLCDControlv_1.0fancystart/useDefLCDControlv_1.0fancystart.ino
@@ -0,0 +1,65 @@
+
+
+// Setup LCD screen
+#include <Wire.h> // Include wire (i2c comm) and liquidcrystal libraries
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // object declaration 20x4 LCD screen, 0x27 is the address
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  lcdSetup(); // ;)
+}
+
+void lcdSetup(){ //setup lcd with fancy display
+  int ppos = 4; //counter position for cursor to print periods
+  int cpos = 4; //counter pos for cursor to print string
+  int npos = 0; //counter for position within string
+  int i; //alternating incrementation
+  String nacahd = "N.A.C.A.H.D."; //creates nacahd string to be referenced
+  lcd.init();
+  lcd.backlight();
+  //this loop prints periods, notice total chars/2 because we print 2 chars each loop
+  for (int poscount=0; poscount<6; poscount++){ 
+    lcd.setCursor(ppos, 1); //counter moves cursor 
+    lcd.print(" .");
+    ppos += 2; //cursor moves 2 spaces because there are 2 characters
+    delay(150); //mess with this to best dramatic effect
+    }
+    delay(800);
+  //this loop prints NACAHD with each individual letter printed separately using string position calling (npos)
+  for (int poscount=0; poscount<6; poscount++){ 
+    //lcd.scrollDisplayLeft();
+    lcd.setCursor(cpos, 1); //counter moves cursor
+    lcd.print(nacahd.charAt(npos)); //prints the character referenced by the position (npos) in nacahd string
+    npos += 2; //iterate counters 
+    cpos += 2;
+    delay(150);
+  }
+  delay(800);
+  //this loop blinks the word "initializing"
+  for (int i=0; i<6; i++){ //
+    if (i%2==1){ //modulo to check whether i is odd to change between 2 states
+      lcd.setCursor(3,2);
+      lcd.print("Initiating...");
+      delay(500);
+     }
+     else{
+      lcd.setCursor(3,2);
+      lcd.print("             ");
+      delay(380);
+     }
+     
+  }
+  lcd.clear();
+}
+
+void lcdUpdate () {
+
+
+  
+}
