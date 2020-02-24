d82d3ae2bcc20c5d3051d50d54a4e27d3e55970b
10bulls10 <sspitzer@pdx.edu>
2020-02-24 11:24:32 -0800

    temp calibration data added to calibration folder

--- Calibration/tempCalibrationCode/tempCalibrationCode.ino
+++ Calibration/tempCalibrationCode/tempCalibrationCode.ino
@@ -1,17 +1,27 @@
+// Setup LCD screen
+#include <Wire.h> // Include wire (i2c comm) and liquidcrystal libraries
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // 20x4 LCD screen, 0x27 is the address
 
-int tempPin = 5;
+
+
+int tempPin = A1;
 void setup() {
   
   Serial.begin(9600);
   pinMode(tempPin, INPUT);
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
   
   int reading;
-
+  delay(25);
   reading = analogRead(tempPin);
   Serial.println(reading);
-  delay(25);
+  lcd.setCursor(3, 1);
+  lcd.print(reading);
+  
   
 }
